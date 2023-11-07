d456f9ec6305e0b96df03c09f04e944819e71fe4
tomathyg <tomathyg@hotmail.com>
2023-11-07 20:38:21 +0000

    Brevo Signup Form

---
+++ app/components/BrevoSignupForm.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+export default function BrevoSignupForm () {
+    return (
+        <iframe
+        width="540"
+        height="305"
+        src="https://83dff8dc.sibforms.com/serve/MUIFAF4SDnQz8BDnsEiJsnSTa1r1LFpJA4YwwA476AqCOJd9FFFRJg0i6_gm4HYCersWSfYl7s8HRUBeqoBelWazt7oBo3nFME93thVJ4iuPekCZi7dkCDlVSCWoj76TCef1B78V6LtZ1QCb3swfga1Gm9X92O06CKL6FIrZvnU-T0LhGjLnrL9LA2mYoPI7yxitbPGMdYlcar8i"
+        frameBorder="0"
+        scrolling="auto"
+        allowFullScreen
+        style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', maxWidth: '100%'}}
+        ></iframe>
+
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 import { draftMode } from 'next/headers'
 
+import BrevoSignupForm from './components/BrevoSignupForm'
+
 function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="flex-col flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Sound of Fractures
       </h1>
