b6e5c9f4c24541d502dcf26330294d78690629ea
tomathyg <tomathyg@hotmail.com>
2023-11-11 12:16:35 +0000

    date edited

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ function Header({heading, text}: HeaderProps) {
         <ScenesLogoWhite />
       </h1>
       </div>
-      <p className="text-2xl mt-4 leading-10">{/*{text}*/}BEGINNING<br />01/12/23</p>
+      <p className="text-2xl mt-4 leading-10">{/*{text}*/}BEGINNING<br />1/12/23</p>
     </section>
   )
 }
