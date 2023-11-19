f3acf76b1c06c86540fb7c35aa79827157d89711
tomathyg <tomathyg@hotmail.com>
2023-11-19 23:57:16 +0000

    pages text center

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
     return (
-        <section className="flex justify-center">
+        <section className="flex justify-center text-center">
             <h1 className="text-8xl">ABOUT</h1>
         </section>
     )
--- app/scenes/page.tsx
+++ app/scenes/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
     return (
-        <section className="flex justify-center">
+        <section className="flex justify-center text-center">
             <h1 className="text-8xl">ALL SCENES</h1>
         </section>
     )
