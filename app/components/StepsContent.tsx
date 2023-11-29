e23781a42966aec8453dd681a83c1b96149eff42
tomathyg <tomathyg@hotmail.com>
2023-11-29 17:08:08 +0000

    about and process content

--- app/components/StepsContent.tsx
+++ app/components/StepsContent.tsx
@@ -25,31 +25,30 @@ export default function Content() {
         </section>
         <section className="mt-4 px-8 overlap-above content-section relative grid grid-cols-1 sm:grid-cols-3 gap-x-12 px-4">
             <div className="w-full flex flex-col mb-8">
-                <div className='column-image-container aspect-video relative overflow-hidden rounded-xl'>
-                    <Image
+                <div className='column-image-container aspect-landscape relative overflow-hidden rounded-xl bg-orange'>
+                    {/*<Image
                         //loader={imageLoader}
-                        /*width='300'
-                        height='200'*/
                         fill={true}
                         src='/images/1.jpg'
                         alt=''
                         //placeholder='blur'
                         quality={75}
-                    />
+                    />*/}
                 </div>
                 <div className='column-text-container text-center text-xl'>
                     <h2 className='mt-8'>01 - LISTEN & READ</h2>
                     <p className='font-serif'>LISTEN TO THE SONG AND READ THE PROMPT</p>
+                    <p className='font-mono text-xs mt-4'>UPLOAD THE IMAGE OF YOUR MEMORY, AND ENTER YOUR RESPONSE TO THE PROMPT</p>
                 </div>
             </div>
             <div className="w-full flex flex-col mb-8">
-                <div className='column-image-container aspect-video relative overflow-hidden rounded-xl'>
+                <div className='column-image-container aspect-landscape relative overflow-hidden rounded-xl'>
                     <Image
                         //loader={imageLoader}
                         /*width='300'
                         height='200'*/
                         fill={true}
-                        src='/images/1.jpg'
+                        src='/images/process-step-2-enter-caption.png'
                         alt=''
                         //placeholder='blur'
                         quality={75}
@@ -58,16 +57,17 @@ export default function Content() {
                 <div className='column-text-container text-center text-xl'>
                     <h2 className='mt-8'>02 - UPLOAD</h2>
                     <p className='font-serif'>UPLOAD THE IMAGE OF YOUR MEMORY, AND ENTER YOUR RESPONSE TO THE PROMPT</p>
+                    <p className='font-mono text-xs mt-4'>UPLOAD THE IMAGE OF YOUR MEMORY, AND ENTER YOUR RESPONSE TO THE PROMPT</p>
                 </div>
             </div>
             <div className="w-full flex flex-col mb-8">
-                <div className='column-image-container aspect-video relative overflow-hidden rounded-xl'>
+                <div className='column-image-container aspect-landscape relative overflow-hidden rounded-xl'>
                     <Image
                         //loader={imageLoader}
                         /*width='300'
                         height='200'*/
                         fill={true}
-                        src='/images/1.jpg'
+                        src='/images/process-step-3-example-scene.jpg'
                         alt=''
                         //placeholder='blur'
                         quality={75}
@@ -76,6 +76,7 @@ export default function Content() {
                 <div className='column-text-container text-center text-xl'>
                     <h2 className='mt-8'>03 - FINAL SCENE</h2>
                     <p className='font-serif'>YOUR IMAGE WILL BE PROCESSED, MINTED AND ENTERED INTO THE SELECTION PROCESS</p>
+                    <p className='font-mono text-xs mt-4'>UPLOAD THE IMAGE OF YOUR MEMORY, AND ENTER YOUR RESPONSE TO THE PROMPT</p>
                 </div>
             </div>
         </section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,11 +14,18 @@ export default {
         mono: ['var(--font-dm-mono)'],
         serif: ['var(--font-gambarino)'],
       },
+      fontSize: {
+        xsm: '0.5rem',
+      },
       colors: {
         'off-white': '#e8e0c5',
+        'orange': '#d6301a',
       },
       screens: {
-        'xsm': '480px'
+        'xsm': '480px',
+      },
+      aspectRatio: {
+        'landscape': '1.5 / 1',
       }
     },
   },
