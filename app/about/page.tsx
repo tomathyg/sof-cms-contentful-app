fced84e57a49b59903cbf541236d3270a31bdde4
tomathyg <tomathyg@hotmail.com>
2023-11-30 00:36:18 +0000

    about title

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,7 @@ export default async function Page() {
     return (
         <section className="flex justify-center w-full px-8">
             <div className="flex flex-col items-center text-center max-w-prose tracking-wide uppercase">
-                <h1 className="text-4xl xsm:text-6xl sm:text-8xl my-8">ABOUT</h1>
+                <h1 className="text-4xl xsm:text-6xl sm:text-8xl my-8">{content.title}</h1>
                 <p className="font-sans text-2xl mb-4">{content.paragraph1}</p>
                 <p className="font-serif text-2xl mb-4">{content.paragraph2}</p>
                 <p className="font-mono text-xsm mb-4">{content.paragraph3}</p>
