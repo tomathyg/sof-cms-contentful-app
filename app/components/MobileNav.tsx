cadb2493e0afe1395a34a6df7814f1706da60ba3
tomathyg <tomathyg@hotmail.com>
2023-12-09 18:00:21 +0000

    mobile nav opacity

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -38,7 +38,7 @@ const MobileNav = () => {
       <button onClick={() => setIsOpen(!isOpen)} className={`font-sans mobile-nav-button flex items-center z-10 h-[30px] w-[30px] overflow-visible ${isOpen ? 'active' : ''}`}>
         {/*<svg id="burger-square" viewBox="0 -20 118 158"><rect y="74.12" width="118" height="43.88" fill="#e8e0c5"/><rect y="31.39" width="118" height="32.54" fill="#e8e0c5"/><rect width="118" height="21.21" fill="#e8e0c5"/></svg>*/}MENU
       </button>
-      <div ref={menuRef} className='fixed left-0 top-0 w-full h-full pt-[77px] pl-6 bg-black font-sans text-xl leading-9'>
+      <div ref={menuRef} className='fixed left-0 top-0 w-full h-full pt-[77px] pl-6 bg-black font-sans text-xl leading-9 opacity-0'>
         <ul>
           <li><button type="button" onClick={() => handleNavigation('/')}>HOME</button></li>
           <li><button type="button" onClick={() => handleNavigation('/about')}>ABOUT</button></li>
