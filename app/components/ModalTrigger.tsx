59a8ec67f5487cee87cb07394274f96aec8f7c4d
tomathyg <tomathyg@hotmail.com>
2024-04-14 16:53:17 +0100

    sign up

--- app/components/ModalTrigger.tsx
+++ app/components/ModalTrigger.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Home() {
     return (
         <>
-            <div className="fixed z-20 right-8 sm:right-12 bottom-16">
+            <div className="fixed z-10 right-8 sm:right-6 bottom-16">
                 <Link href="?modal=true">
                     <button type="button" className="bg-black text-xsm xsm:text-base text-off-white p-2 font-sans uppercase border border-off-white rounded-lg py-2 px-4">
-                        <span>JOIN</span>
+                        <span>SIGN UP</span>
                     </button>
                 </Link>
             </div>
