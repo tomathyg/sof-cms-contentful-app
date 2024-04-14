313866c41f7c7913fcaa02386a875c8f6ba64430
tomathyg <tomathyg@hotmail.com>
2024-04-14 17:31:34 +0100

    button position

--- app/components/ModalTrigger.tsx
+++ app/components/ModalTrigger.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
     return (
         <>
-            <div className="fixed z-10 right-6 sm:right-6 bottom-16">
+            <div className="fixed z-10 right-6 sm:right-6 bottom-[4.5rem]">
                 <Link href="?modal=true">
                     <button type="button" className="bg-black text-xsm xsm:text-base text-off-white p-2 font-sans uppercase border border-off-white rounded-lg py-2 px-4">
                         <span>SIGN UP</span>
