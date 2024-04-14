bb08873f9835fd03c8197f74fff533b7f4db96dd
tomathyg <tomathyg@hotmail.com>
2024-04-14 20:21:39 +0100

    mobile menu

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -46,6 +46,8 @@ const MobileNav = () => {
           <li><button type="button" onClick={() => handleNavigation('/scenes/scene-1')}>BUY SCENE 1</button></li>
           <li><button type="button" onClick={() => handleNavigation('/scenes/scene-2')}>BUY SCENE 2</button></li>
           <li><button type="button" onClick={() => handleNavigation('/scenes/scene-3')}>BUY SCENE 3</button></li>
+          <li><button type="button" onClick={() => handleNavigation('/scenes/scene-4')}>BUY SCENE 4</button></li>
+          <li><button type="button" onClick={() => handleNavigation('/scenes/scene-5')}>BUY SCENE 5</button></li>
           <li><button type="button" onClick={() => handleNavigation('/about')}>ABOUT</button></li>
           <li><button type="button" onClick={() => handleNavigation('/credits')}>CREDITS</button></li>
         </ul>
